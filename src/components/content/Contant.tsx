aa497072e14716209f1c88375890872806501659
Andriikryl <andreyk557@gmail.com>
2023-12-03 01:03:50 +0200

    add types

--- src/components/content/Contant.tsx
+++ src/components/content/Contant.tsx
@@ -6,7 +6,20 @@ export default function Contant() {
   return (
     <section>
       <Container>
-        <div></div>
+        <div>
+          <div className={styles.wrapper}>
+            <h3 className={styles.title}>Data types in JavaScript?</h3>
+            <p>The Undefined Type</p>
+            <p>The Null Type</p>
+            <p>The Boolean Type</p>
+            <p>The String Type</p>
+            <p>The Symbol Type</p>
+            <p>Numeric Types</p>
+            <p>The Number Type</p>
+            <p>The BigInt Type</p>
+            <p>The Object Type</p>
+          </div>
+        </div>
       </Container>
     </section>
   );
